d08c42a36a2038dbc8c3c4932dbda84ff6cff489
kevin arnold flores pacheco <arnold_k2001@hotmail.com>
2024-11-18 06:02:44 -0500

    agregando setup de pinia persistent, lucide icons y bcryptjs

--- src/main.js
+++ src/main.js
@@ -1,11 +1,19 @@
-import { createApp } from 'vue';
+import { createApp, markRaw } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
 import './style.css'; // Importamos Tailwind CSS
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+
+const pinia = createPinia();
+
+pinia.use(({ store }) => {
+    store.router = markRaw(router)
+})
+
+pinia.use(piniaPluginPersistedstate)
 
 const app = createApp(App);
-const pinia = createPinia();
 
 app.use(pinia); // Usamos Pinia
 app.use(router); // Usamos Vue Router

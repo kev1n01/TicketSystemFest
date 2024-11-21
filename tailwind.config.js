65ae2ea2bea3d5629a2e4443d0d9eedda0a31a97
kevin arnold flores pacheco <arnold_k2001@hotmail.com>
2024-11-21 12:33:04 -0500

    setup de fuente y text shadow con tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
         sans: ['Arial', 'sans-serif'],
         epilogue: ['Epilogue', 'sans-serif'],
         russo: ['"Russo One"', 'sans-serif'],
+        dastress: ['"Dastress"', 'sans-serif'],
       },
       screens: { // tamaños de pantalla personalizados
         'xs': '360px',
@@ -20,8 +21,22 @@ module.exports = {
         'xl': '1280px',
         '2xl': '1536px',
       },
-
+      keyframes: {
+        pulse2: {
+          '50%': {
+            opacity: '.8',
+          },
+        },
+      },
+      animation: {
+        pulse2: 'pulse2 3s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      },
+      textShadow: {
+        redGlow: '0 0 10px #da4646, 0 0 20px #da4646, 0 0 30px #da4646, 0 0 40px #da4646, 0 0 50px #da4646, 0 0 60px #da4646, 0 0 70px #da4646',
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-textshadow'),
+  ],
 };

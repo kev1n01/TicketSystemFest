a17a050d7d7d075e6e00edb1ad699d120475093b
kevin arnold flores pacheco <arnold_k2001@hotmail.com>
2024-11-21 12:38:30 -0500

    adaptando imagenes para llamas desde public

--- src/components/Sponsors.vue
+++ src/components/Sponsors.vue
@@ -2,61 +2,61 @@
 const logosSponsors = [
     {
         name: "LTD UDH",
-        src: "src/assets/imgs/ltd.png",
+        image: "ltd.png",
         url: "https://www.facebook.com/UDHPAISI",
         alt: "Laboratorio de transformación digital UDH"
     },
     {
         name: "Paisi UDH",
-        src: "src/assets/imgs/paisi.png",
+        image: "paisi.png",
         url: "https://www.facebook.com/UDHPAISI",
         alt: "Paisi UDH"
     },
     {
         name: "VRI UDH",
-        src: "src/assets/imgs/vri.png",
+        image: "vri.png",
         url: "https://www.facebook.com/investigacionudh",
-        alt: "Vicerrectorado de investigación UDH"	
+        alt: "Vicerrectorado de investigación UDH"
     },
     {
         name: "Makita makis express ",
-        src: "src/assets/imgs/makita.jpg",
+        image: "makita.jpg",
         url: "https://www.facebook.com/makitamakisexpress",
         alt: "Makita makis express"
     },
     {
         name: "Polleria Chiwchi",
-        src: "src/assets/imgs/chiwchi.jpg",
+        image: "chiwchi.jpg",
         url: "https://www.facebook.com/Chiwchialitas",
         alt: "Makita makis express"
     },
     {
         name: "El blog de juancho",
-        src: "src/assets/imgs/juancho.png",
+        image: "juancho.png",
         url: "https://www.youtube.com/@ELBLOGDEJUANCHO",
         alt: "El blog de juancho"
     },
     {
         name: "Resto bar chill out",
-        src: "src/assets/imgs/chill_out.jpg",
+        image: "chill_out.jpg",
         url: "https://www.facebook.com/ChillOutHuanuco",
         alt: "Resto bar chill out"
     },
     {
         name: "Cilindro central",
-        src: "src/assets/imgs/cilindro_central.jpg",
+        image: "cilindro_central.jpg",
         url: "https://www.facebook.com/restaurantecilindrocentral",
         alt: "Cilindro central"
     },
     {
         name: "Pidelo huanuco",
-        src: "src/assets/imgs/pidelo.jpg",
+        image: "pidelo.jpg",
         url: "https://www.facebook.com/PideloHuanuco",
         alt: "Pidelo huanuco"
     },
     {
         name: "Dfabios comida rapida",
-        src: "src/assets/imgs/dfabios.jpg",
+        image: "dfabios.jpg",
         url: "https://www.facebook.com/DFABIOS",
         alt: "Dfabios comida rapida"
     },
@@ -71,8 +71,8 @@ const logosSponsors = [
                 <template v-for="(ls, index) in logosSponsors" :key="index">
                     <div
                         class="bg-white backdrop-blur-sm p-4 rounded-xl hover:shadow-xl hover:shadow-red-500/60 transition-all hover:-translate-y-1">
-                        <a :href="ls.url" target="_blank" :title="ls.name">
-                            <img :src="ls.src" :alt="ls.src" class="w-full rounded-lg">
+                        <a :href="ls.url" target="_blank">
+                            <img :src="`/imgs/${ls.image}`" :alt="ls.name" class="w-full rounded-lg">
                         </a>
                     </div>
                 </template>

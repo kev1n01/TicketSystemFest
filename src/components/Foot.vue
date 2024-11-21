e223570d0590120f18efecc90dc046e0ef12152d
kevin arnold flores pacheco <arnold_k2001@hotmail.com>
2024-11-21 12:34:19 -0500

    quitando icono de instagram  y agregando tipo de fuente

--- src/components/Foot.vue
+++ src/components/Foot.vue
@@ -8,13 +8,10 @@ const yearCurrent = ref(new Date().getFullYear())
     <footer class="flex justify-between flex-col space-y-3 md:flex-row text-center border-t border-gray-700/50 py-10 items-center content-center">
         <div class="flex items-center">
             <a href="#header" class="text-2xl">
-                <p class="text-2xl font-russo">SystemFest</p>
+                <p class="text-2xl font-dastress tracking-wider">SystemFest</p>
             </a>
             <a href="https://github.com/Transformacion-Digital-UDH" target="_blank">
                 <Github class="ml-8 cursor-pointer hover:stroke-red-400" />
-            </a>
-            <a href="https://www.instagram.com/kkevv1" target="_blank">
-                <InstagramIcon class="ml-2 cursor-pointer hover:stroke-red-400" />
             </a>
         </div>
         <p class="text-gray-400">© {{ yearCurrent }} System Fest - LTD - UDH. Todos los derechos reservados.
